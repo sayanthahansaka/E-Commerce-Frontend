2254034585b24d5dd886c4fe69e0a891add0c60b
Sayantha Hansaka <sayanthahansaka00@gmail.com>
2023-12-11 15:14:04 +0530

    Create Pages JSX fils

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { BrowserRoutern,Routes,Route} from 'react-router-dom';
+import Product from './Pages/Product';
 
 function App() {
   return (
     <div>
+      <BrowserRoutern>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/mens' element={<ShopCategory category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<LoginSignup/>}/>
+      </Routes>
+      </BrowserRoutern>
     </div>
   );
 }
